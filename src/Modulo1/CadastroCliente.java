8a5b56af4cd52b33f146b87916cb0ff6719a3622
Bruna Bittencourt <bruna.costta39@gmail.com>
2021-09-17 18:12:02 -0300

    Aula 17.09

--- src/Modulo1/CadastroCliente.java
+++ src/Modulo1/CadastroCliente.java
@@ -1,5 +1,27 @@
 package Modulo1;
 
+import java.util.Scanner;
+
 public class CadastroCliente {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\tCadastro de Cliente");
+        System.out.println("\t1 - Cadastrar cliente: ");
+        System.out.println("\t2 - Sair");
+        System.out.println("Escolher uma opção");
+
+        String nome, sobrenome;
+        int op = Integer.parseInt(sc.nextLine());
+        switch (op){
+            case 1:
+                System.out.print("\tDigite o nome do cliente: ");
+                nome = sc.nextLine();
+            break;
+            case 2:
+                System.out.print("\tEspero que voce volte na proxima ");
+            break;
+        }
+    }
     
 }
